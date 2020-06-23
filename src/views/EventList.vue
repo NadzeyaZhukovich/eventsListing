60e04fd7d437474f7d971c2f4921793ff039d416
nadzeyazhukovich <nadzeya.zhukovich@trimm.nl>
2020-06-23 20:27:42 +0200

    Added axios

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
     <h1>Events Listening</h1>
-    <EventCart />
+    <EventCart v-for="event in events" :key="event.id" :event="event"/>
   </div>
 </template>
 
 <script>
 import EventCart from '@/components/EventCart';
+import EventService from "../services/EventService";
+
 export default {
-  components: { EventCart }
+  components: { EventCart },
+  data() {
+    return {
+      events: []
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then(responce => {
+        this.events = responce.data
+      })
+      .catch( error => {
+        console.log('There was an error:' + error.response)
+      })
+  }
 };
 </script>
