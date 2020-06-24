982cac719baaac44030f9a86d06346ffc815f7c8
nadzeyazhukovich <nadzeya.zhukovich@trimm.nl>
2020-06-24 12:59:11 +0200

    Added store; mapping state and getters to computed;

--- src/views/CreateEvents.vue
+++ src/views/CreateEvents.vue
@@ -1,3 +1,18 @@
 <template>
-    <h1>Create Events</h1>
+  <div>
+    <h1>Create Events {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ getEventById(2) }} categories</p>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['getEventById']),
+    ...mapState(['user', 'categories'])
+  }
+};
+</script>
